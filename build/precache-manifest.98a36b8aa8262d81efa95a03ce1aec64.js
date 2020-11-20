4f3bec43bdb48e2f359aafdec8b0a038f63905bc
jack-pults <jack.pults@gmail.com>
2020-11-20 14:28:58 -0600

    Add files via upload

---
+++ build/precache-manifest.98a36b8aa8262d81efa95a03ce1aec64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf8d06734705d48400dbac2888554b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13fae66073fca9aeadc9",
+    "url": "/static/css/main.2f8a371d.chunk.css"
+  },
+  {
+    "revision": "f7f9ea4b55d2dde13f08",
+    "url": "/static/js/2.758e9b06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.758e9b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13fae66073fca9aeadc9",
+    "url": "/static/js/main.b35f22b9.chunk.js"
+  },
+  {
+    "revision": "2d9417c07231f4c71ce2",
+    "url": "/static/js/runtime-main.c08ed251.js"
+  }
+]);
